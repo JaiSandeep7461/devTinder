f4de5d523d8d2e530a01408a494b064fb2d1cd9d
JaiSandeep7461 <jaisandeep7461@gmail.com>
2025-05-05 22:21:15 +0530

    Exploring GET,POST and PATCH Methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 
 const app = express();
+
+
+app.get("/hello",(req,res)=>{
+  res.send({"firstName":"Sandeep","lastName":"Peela"});
+  });
+
+app.post("/user",(req,res)=>{
+  console.log("Save Data to the database");
+  res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("Deleted Successfully!");
+});
+
 app.use((req, res) => {
   res.send("Hello from the Namste Server");
 });
+
+
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
 });
