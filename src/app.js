d40844375320d0275daf663d2ca41082cce4c779
JaiSandeep7461 <jaisandeep7461@gmail.com>
2025-05-11 00:50:55 +0530

    Middleware Test done using multiple route control methods

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 
 
-app.get("/hello",(req,res)=>{
+
+
+/* app.get("/abc",(req,res)=>{
+  console.log("Req >>>", req.query);
   res.send({"firstName":"Sandeep","lastName":"Peela"});
   });
 
@@ -14,11 +17,28 @@ app.post("/user",(req,res)=>{
 
 app.delete("/user",(req,res)=>{
   res.send("Deleted Successfully!");
-});
+}); */
 
-app.use((req, res) => {
-  res.send("Hello from the Namste Server");
-});
+app.use("/user",(req, res,next) => {
+  console.log("Handling the route user!!");
+  next();
+},
+(req,res,next)=>{
+  console.log("Handling the route user2");
+  // res.send("2nd Response");
+  next();
+},
+(req,res,next)=>{
+  console.log("Handling the route user 3");
+  // res.send("3rd Response");
+  next();
+},
+(req,res,next)=>{
+  console.log("Handling the route user 4");
+  res.send("4th response");
+
+}
+);
 
 
 app.listen(3000, () => {
