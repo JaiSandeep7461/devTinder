1b0fbb85f3d755a8d2ed0bc8850862e475f4ca9c
JaiSandeep7461 <jaisandeep7461@gmail.com>
2025-05-11 01:53:59 +0530

    Error handling implementation Done

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-
-app.get("/admin/getAllData",adminAuth)
-
-app.get("/admin/deleteUser",(req,res)=>{
-  res.send("Deleted a user");
+app.get("/user", (req, res) => {
+  throw new Error("hksdfjsd");
+  res.send("User Data Sent");
 });
 
+// The below method is use to handle all errors and it will match all the routes
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
