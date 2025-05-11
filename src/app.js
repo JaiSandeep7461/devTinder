001ce871c2aeca1224f2498736b0e53f972697d6
JaiSandeep7461 <jaisandeep7461@gmail.com>
2025-05-11 13:24:19 +0530

    Dynamic data Signup

--- src/app.js
+++ src/app.js
@@ -3,34 +3,25 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-
-app.post("/signUp",async(req,res)=>{
-
-
+app.use(express.json());
+app.post("/signUp", async (req, res) => {
   //Creating a new instance of the User Model
-  const user = new User({
-    firstName:"Test1",
-    lastName:"sample",
-    emailId:"test45@gmail.com",
-    password:"Test@1234"
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 3000");
+    });
+  })
+  .catch(err => {
+    console.error("Database cannot be connected");
   });
- try{
-  await user.save();
- res.send("User Added Successfully");
- }catch(err){
-  res.status(400).send("Error saving the user: "+err.message);
- }
-
-})
-
-
-connectDB().then(()=>{
-  console.log("Database connection established");
-  app.listen(7777, () => {
-    console.log("Server is successfully listening on port 3000");
-  });
-}).catch(err=>{
-  console.error("Database cannot be connected");
-})
-
-
