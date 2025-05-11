550f592f8b9c02a8ba0e34d59ea6068d9fd55839
JaiSandeep7461 <jaisandeep7461@gmail.com>
2025-05-11 17:42:26 +0530

    Validations Done for fields

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');   
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -15,11 +16,21 @@ const userSchema = new mongoose.Schema({
         lowercase:true,
         required:true,
         unique:true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email Address");
+            }
+        }
     },
     password:{
         type:String,
-        required:true
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("use Strong Password");
+            }
+        }
     },
     age:{
         type:Number,
@@ -35,7 +46,12 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl:{
         type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF02Jj8T2t7PdkytAw42HDuuSz7yXguKn8Lg&s"
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF02Jj8T2t7PdkytAw42HDuuSz7yXguKn8Lg&s",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid Photo  URL");
+            }
+        }
     },
     about:{
         type:String,
